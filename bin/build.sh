b9d32ba4f28cbcd250954a378e134e87072a55a9
littensy <56808540+littensy@users.noreply.github.com>
2025-03-09 15:29:16 -0700

    build: create project files for release

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,14 @@
 mkdir build
-rojo build packages/charm -o build/charm.rbxm
-rojo build packages/charm-sync -o build/charm-sync.rbxm
-rojo build packages/react -o build/react-charm.rbxm
-rojo build packages/vide -o build/vide-charm.rbxm
+
+build() {
+	cat > packages/default.project.json <<EOF
+{"name":"$1","globIgnorePaths":["**/node_modules","**/package.json","**/wally.toml"],"tree":{"\$path":"$1"}}
+EOF
+	rojo build packages -o build/$1.rbxm
+	rm -rf packages/default.project.json
+}
+
+build charm
+build charm-sync
+build react
+build vide
