51579750911efcc5bc77de5a772d84a282035698
littensy <56808540+littensy@users.noreply.github.com>
2026-02-17 17:47:05 -0800

    chore: fix react tests, run stylua

--- packages/charm/src/index.d.ts
+++ packages/charm/src/index.d.ts
@@ -76,7 +76,10 @@ export function signal<T>(
 	equals?: Equals<T>,
 ): LuaTuple<[getter: () => T, setter: (newValue: T | ((currentValue: T) => T)) => T]>;
 export function signal<T>(): LuaTuple<
-	[getter: () => T | undefined, setter: (newValue: T | undefined | ((currentValue?: T) => T | undefined)) => T | undefined]
+	[
+		getter: () => T | undefined,
+		setter: (newValue: T | undefined | ((currentValue?: T) => T | undefined)) => T | undefined,
+	]
 >;
 
 /**
