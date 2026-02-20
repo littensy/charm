69d47d580cf2ae143f2c6936c6e1bbe816ed81cf
littensy <56808540+littensy@users.noreply.github.com>
2026-02-19 21:55:11 -0800

    ci: use javascript eslint config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const eslint = require("@eslint/js");
+const { defineConfig, globalIgnores } = require("eslint/config");
+const prettierRecommended = require("eslint-plugin-prettier/recommended");
+const roblox = require("eslint-plugin-roblox-ts");
+const tseslint = require("typescript-eslint");
+
+module.exports = defineConfig(
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	roblox.configs.tsRecommendedCompat,
+	roblox.configs.recommended,
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"roblox-ts/no-any": "off",
+		},
+	},
+	globalIgnores(["out/**", "config.js"]),
+	prettierRecommended,
+);
